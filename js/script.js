371f9a043e75b5470ff2ed72f0b3590de545c4d1
perezalexreal <perezalexreal@gmail.com>
2021-12-02 13:44:16 +0100

    Finito

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let nome = prompt("Inserisci il tuo nome");
+
+let cognome = prompt("Inserisci il tuo cognome");
+
+let favoriteColor = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById("nome").innerHTML = nome;
+
+document.getElementById("cognome").innerHTML = cognome;
+
+document.getElementById("fav-color").innerHTML = favoriteColor;
